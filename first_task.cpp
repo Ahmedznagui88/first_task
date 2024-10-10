a92214cce8d6365645a01074b7ad314ac856dd29
Ahmed Znagui <amed.bob88@gmail.com>
2024-10-10 21:29:18 +0200

    create bool expression

--- first_task.cpp
+++ first_task.cpp
@@ -7,17 +7,19 @@ int main()
     cout << "Enter 3 letters: " << endl;
     cin >> a >> b >> c;
 
-    bool firstExp = (a >= 97);
-    bool secondExp = (a < 110);
+    bool firstExp = (a >= 97) && (b >= 97) && (c >= 97);
+    bool secondExp = (a < 110) && (b < 110) && (c < 110);
 
-    if (firstExp && secondExp) {
+    if (firstExp && secondExp)
+    {
         a += 13;
-        
-        cout << "ROT13: " << a << endl;
-
-    } else{
+        b += 13;
+        c += 13;
+        cout << "ROT13: " << a << " " << b << " " << c << endl;
+    }
+    else
+    {
         cout << "ROT13: ? " << a << endl;
-
     }
     return 0;
 }
