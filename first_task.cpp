9b3aebb33d920e105a9740821e241f0c72c964e9
Ahmed Znagui <amed.bob88@gmail.com>
2024-10-10 21:43:47 +0200

    exception doesn't work

--- first_task.cpp
+++ first_task.cpp
@@ -9,17 +9,25 @@ int main()
 
     bool firstExp = (a >= 97) && (b >= 97) && (c >= 97);
     bool secondExp = (a < 110) && (b < 110) && (c < 110);
+    bool thirdExp = (a >= 110) && (b >= 110) && (c >= 110);
+    bool fourthExp = (a <= 122) && (c <= 122) && (c <= 122);
 
-    if (firstExp && secondExp)
-    {
+    if (firstExp && secondExp){
         a += 13;
         b += 13;
         c += 13;
         cout << "ROT13: " << a << " " << b << " " << c << endl;
+    } else if (thirdExp && fourthExp){
+        a -= 13;
+        b -= 13;
+        c -= 13;
+        cout << "ROT13: " << a << " " << b << " " << c << endl;
+    } else {
+        (b >= 97 && b <= 122) ? b : '?'; 
+        (b >= 97 && b <= 122) ? b : '?';        
+        (c >= 97 && c <= 122) ? c : '?'; 
     }
-    else
-    {
-        cout << "ROT13: ? " << a << endl;
-    }
+    
     return 0;
+
 }
