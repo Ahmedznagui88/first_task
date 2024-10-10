88835ba0155991c32af223be2e6687abfbe265ad
Ahmed Znagui <amed.bob88@gmail.com>
2024-10-10 21:16:05 +0200

    first draft

--- first_task.cpp
+++ first_task.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
+    char a, b, c;
+    cout << "Enter 3 letters: " << endl;
+    cin >> a >> b >> c;
 
+    bool firstExp = (a >= 97);
+    bool secondExp = (a < 110);
 
+    if (firstExp && secondExp) {
+        a += 13;
+        
+        cout << "ROT13: " << a << endl;
 
+    } else{
+        cout << "ROT13: ? " << a << endl;
+
+    }
     return 0;
 }
