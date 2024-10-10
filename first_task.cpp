720f1bc33d08c67425c19ed2eb4c9e057a97e830
Ahmed Znagui <amed.bob88@gmail.com>
2024-10-10 22:58:17 +0200

    exp still doesn't work

--- first_task.cpp
+++ first_task.cpp
@@ -7,29 +7,30 @@ int main()
     cout << "Enter 3 letters: " << endl;
     cin >> a >> b >> c;
 
-    double num = 122 - 97;
-
     bool firstAndSecond = (a >= 97 && a < 110) && (b >= 97 && b < 110) && (c >= 97 && c < 110);
     bool thirdAndFourth = (a >= 110 && a <= 122) && (b >= 110 && b <= 122) && (c >= 110 && c <= 122);
 
-    if (firstAndSecond){
+    double num = (firstAndSecond - 97 + 13) % 26 + 97 &&
+                 (thirdAndFourth - 97 + 13) % 26 + 97;
+
+    if (firstAndSecond) {
         a += 13;
         b += 13;
         c += 13;
         cout << "ROT13: " << a << " " << b << " " << c << endl;
-    } else if (thirdAndFourth){
+
+    } else if (thirdAndFourth) {
         a -= 13;
         b -= 13;
         c -= 13;
         cout << "ROT13: " << a << " " << b << " " << c << endl;
-    } else if(firstAndSecond && thirdAndFourth) {
+        
+    } else if (firstAndSecond && thirdAndFourth) {
         cout << "ROT13: ";
-        cout << ((a * num) ? a : '?') << " ";
-        cout << ((b * num) ? b : '?') << " ";
-        cout << ((c * num) ? c : '?') << endl;
-    } else {
-        cout << "ROT13: ?";
+        cout << ((a != num) ? a : '?') << " ";
+        cout << ((b != num) ? b : '?') << " ";
+        cout << ((c != num) ? c : '?') << endl;
     }
-    
+
     return 0;
 }
